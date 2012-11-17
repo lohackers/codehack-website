c7222065de76c24fc04b7c776881085ad2518ae8
Edoardo Rivello <erivello@MacBook-Air-di-Edoardo.local>
2012-11-17 23:41:04 +0100

    add codehack core bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,9 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            
+            # Codehack
+            new CodeHack\CoreBundle\CodeHackCoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/CodeHack/CoreBundle/CodeHackCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CodeHack\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CodeHackCoreBundle extends Bundle
+{
+}
